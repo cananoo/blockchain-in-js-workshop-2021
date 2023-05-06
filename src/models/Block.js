611dbe1d4d2f80c831b5b61703634664371a1e4d
root <2059997196@qq.com>
2023-05-06 23:41:15 +0800

    Block Basis commit(lesson 1)

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,26 @@
+import sha256 from "crypto-js/sha256.js";
+
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
 
   */
-  constructor() {}
+  constructor( blockchain, previousHash, height, data ) {
+    this.blockchain = blockchain
+    this.previousHash = previousHash
+    this.height = height
+    this.data = data
+    this.hash = this.hash()
+  }
+
+     // 2. 定义 hash 函数
+    /*
+        返回当前区块的 hash 值
+     */
+    hash(){
+        return sha256(this.previousHash + this.height + this.data).toString()
+    }
+
 }
 
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,4 +1,7 @@
 // Blockchain
+
+
+
 class Blockchain {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含 
@@ -6,17 +9,36 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
-
+  constructor(name) {
+    this.name = name
+    this.genesis = null
+    this.blocks = {}
+  }
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
   longestChain() {
-
-
-    return []
-  }
+    let longestChain = []
+      var temp = 0
+      var HeightestBlock = null
+      for (let block in this.blocks) {
+          if (this.blocks[block].height >= temp) {
+              temp = this.blocks[block].height
+          }
+      }
+    for (let block in this.blocks) {
+        if (this.blocks[block].height === temp) {
+           HeightestBlock = this.blocks[block]
+            longestChain.push(HeightestBlock)
+              break
+        }
+    }
+    while (this.blocks[HeightestBlock.previousHash]!= null) {
+      longestChain.push(this.blocks[HeightestBlock.previousHash])
+      HeightestBlock = this.blocks[HeightestBlock.previousHash]
+    }
+    return longestChain.reverse()
+    }
 }
-
 export default Blockchain
