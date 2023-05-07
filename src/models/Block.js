55fb2723024e53b420896213a57a0ed37a1600dd
root <2059997196@qq.com>
2023-05-07 15:05:25 +0800

    final version

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,13 @@
+import Blockchain from "./Blockchain.js";
 
 
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-
+        - 区块链
+        - 前一个区块的哈希值
+        - 区块高度
+        - 区块哈希值
   */
   constructor(blockchain, previousHash, height, hash) {
     this.blockchain = blockchain
@@ -11,6 +15,8 @@ class Block {
     this.height = height
     this.hash = hash
   }
+
+
 
 }
 export default Block
